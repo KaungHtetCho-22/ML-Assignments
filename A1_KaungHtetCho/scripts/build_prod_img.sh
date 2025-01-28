d0e211bb709320c31416e139efbed8fabec83d0e
KaungHtetCho-22 <kaunghtetcho.ait@gmail.com>
2025-01-28 15:38:08 +0700

    update file path

--- A1_KaungHtetCho/scripts/build_prod_img.sh
+++ A1_KaungHtetCho/scripts/build_prod_img.sh
@@ -1,6 +1,7 @@
 #!/bin/env bash
 
 docker build \
-	-t a1_assignment:prod \
-	--target prod \
-	-f A1_KaungHtetCho/docker/Dockerfile .
+    -t a1_assignment:prod \
+    --target prod \
+    -f A1_KaungHtetCho/docker/Dockerfile \
+    A1_KaungHtetCho
